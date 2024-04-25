ce9ca08dc2eaf59ee13375828012cdbcfb5efa54
won0730 <163428765+won0730@users.noreply.github.com>
2024-04-26 02:42:32 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for n in range(1, 10):
+    for dan in range(2, 6):
+        print(f"{dan} * {n} = {dan * n:2d}", end="\t")
+    print() 
+
+print()
+
+for n in range(1, 10):
+    for dan in range(6, 10):
+        print(f"{dan} * {n} = {dan * n:2d}", end="\t")
+    print()
